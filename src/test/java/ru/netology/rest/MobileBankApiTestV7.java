be50c73d2791ac3337df44c39da2ee4146a57d35
Tom <gts-2010@mail.ru>
2023-05-06 15:57:26 +0500

    Initial commit

--- src/test/java/ru/netology/rest/MobileBankApiTestV7.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV7.java
@@ -18,6 +18,6 @@ class MobileBankApiTestV1 {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"));
+                .body("data", equalTo("some value"));
     }
 }
